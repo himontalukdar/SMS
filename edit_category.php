e4f62d76dac69b2ca66354298ba6b573a3cb823d
unknown <himontalukdar44@gmail.com>
2024-09-09 00:26:25 +0600

    edit category file id eorror message tomorrow problem slove target

--- edit_category.php
+++ edit_category.php
@@ -2,3 +2,35 @@
     require('connection.php');
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Edit of Category</title>
+</head>
+<body>
+    <?php
+        if (isset($_GET['id'])){
+            $getid = $_GET['id'];
+            $sql = "SELECT * FROM category WHERE category_id=$getid";
+
+            $query + $conn-> query($sql);
+
+            $data = mysqli_fetch_assoc($query);
+            $category_id  = $data['category_id'];
+            $category_name  = $data['category_name'];
+            $category_entrydate = $data['category_entrydate'];
+        }
+            
+    ?>
+    <form action="edit_category.php" method="get">
+        Category : <br>
+        <input type="text" name="category_name" value="<?php echo $category_name?>"><br><br>
+        Category Entry Date : <br>
+        <input type="date" name="category_entrydate" value= "<?php echo $category_entrydate?>"><br><br>
+        <input type="text" name=" category_id" value= "<?php echo  $category_id?>"><br><br>
+        <input type="submit" value="Update">
+    </form>
+</body>
+</html>
--- list_of_category.php
+++ list_of_category.php
@@ -14,12 +14,13 @@
        $query = $conn -> query($sql);
        echo "<table border='1'><tr><th>Category</th><th>Date</th><th>Action</th></tr>";
        while($data =mysqli_fetch_assoc($query)){
-            $category_name = $data['category_name'];
+            $category_id        = $data['category_id'];
+            $category_name      = $data['category_name'];
             $category_entrydate = $data['category_entrydate'];
             echo "<tr>
                     <td>$category_name</a></td>
                     <td>$category_entrydate</td>
-                    <td><a href='edit_category.php'>Edit</a></td>
+                    <td><a href='edit_category.php?id=$category_id'>Edit</a></td>
                     </tr>";
 
        }
