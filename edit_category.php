fc4da4bb74848afe250cd96dd8bcde77e55ec2d3
unknown <himontalukdar44@gmail.com>
2024-09-09 23:44:42 +0600

    update program done

--- edit_category.php
+++ edit_category.php
@@ -15,13 +15,29 @@
             $getid = $_GET['id'];
             $sql = "SELECT * FROM category WHERE category_id=$getid";
 
-            $query + $conn-> query($sql);
+            $query = $conn-> query($sql);
 
             $data = mysqli_fetch_assoc($query);
             $category_id  = $data['category_id'];
             $category_name  = $data['category_name'];
             $category_entrydate = $data['category_entrydate'];
         }
+        if(isset($_GET['category_name'])){
+            $new_category_name       = $_GET['category_name'];
+            $new_category_entrydate = $_GET['category_entrydate'];
+            $new_category_id        = $_GET['category_id'];
+
+
+            $sql1 = "UPDATE category SET 
+            category_name='$new_category_name', 
+            category_entrydate='$new_category_entrydate' WHERE category_id= ' $new_category_id' ";
+            
+           if( $conn-> query($sql1) ===  TRUE){
+                    echo 'Update Successfully';
+           }else{
+            echo 'Not Update ';
+           }
+        } 
             
     ?>
     <form action="edit_category.php" method="get">
@@ -29,7 +45,7 @@
         <input type="text" name="category_name" value="<?php echo $category_name?>"><br><br>
         Category Entry Date : <br>
         <input type="date" name="category_entrydate" value= "<?php echo $category_entrydate?>"><br><br>
-        <input type="text" name=" category_id" value= "<?php echo  $category_id?>"><br><br>
+        <input type="text" name=" category_id" value= "<?php echo  $category_id?>" hidden><br>
         <input type="submit" value="Update">
     </form>
 </body>
